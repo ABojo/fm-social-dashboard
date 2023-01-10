6ba35c1c3f9c5ba7ce51e38a95912f0041ed69f5
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-01-09 21:21:37 -0500

    didnt want to reinvent the wheel so I pulled a working css toggle component and restyled it to fit the needs of this project. Wrote the js to toggle the theme and persist the users choice in localStorage

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const storage = (() => {
+  function isThemeDark() {
+    return JSON.parse(localStorage.getItem("dark"));
+  }
+
+  function toggleTheme() {
+    const themeIsDark = isThemeDark();
+    localStorage.setItem("dark", !themeIsDark);
+  }
+
+  return { isThemeDark, toggleTheme };
+})();
+
+const displayController = (() => {
+  const root = document.documentElement;
+  const themeToggle = document.querySelector(".switch input");
+
+  function toggleTheme() {
+    root.classList.toggle("dark");
+  }
+
+  function enableDarkMode() {
+    root.classList.add("dark");
+    themeToggle.checked = true;
+  }
+
+  function addToggleChangeListener(cb) {
+    themeToggle.addEventListener("change", cb);
+  }
+
+  return { toggleTheme, enableDarkMode, addToggleChangeListener };
+})();
+
+function init() {
+  const themeIsDark = storage.isThemeDark();
+
+  if (themeIsDark) {
+    displayController.enableDarkMode();
+  }
+
+  displayController.addToggleChangeListener(() => {
+    storage.toggleTheme();
+    displayController.toggleTheme();
+  });
+}
+
+init();
